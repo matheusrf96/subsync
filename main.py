cfe3a96f19ed945b8fa4032fa558bfc332b73c75
Matheus R <matheusrf96@gmail.com>
2018-07-15 19:31:04 -0300

    New input text

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from content.io import *
 from content.timeConverter import *
 
 filepath = raw_input("Insert the name of the a file to be updated: ")
-time = input("Insert how many seconds you want to delay(negative number[use '-' before the number]) or haste(positive number[without '+']) the subtitles: ")
+time = input("Change (seconds): [Ex.: +20 | -5] ")
 
 if writeNewFile(getNewFile(filepath, time), filepath):
     print("A new file was created!")
